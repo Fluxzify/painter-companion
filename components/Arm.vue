2de0398384d2d162847f6e2fccc349c49cca4578
josev-dev <josevelasquezguerreroo@gmail.com>
2025-08-08 04:06:13 -0400

    delete of unused arm and body components

--- components/Arm.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <img src="@/assets/Companion/Sprites/Arm/Arm Base.png" :style="{ transform: `rotate(${rotation}deg)` }" />
-</template>
-
-<script setup lang="ts">
-defineProps<{ rotation: number }>()
-</script>
--- components/Body.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <img src="@/assets/Companion/Sprites/Body/Wizard Base.png"  />
-</template>
-
-
-<script setup lang="ts">
-
-</script>
